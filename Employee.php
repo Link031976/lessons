56fb5db19e936892272bee071725bfec883e268e
Alex <link_igor@i.ua>
2022-10-16 13:11:24 +0300

    -2

--- Employee.php
+++ Employee.php
@@ -1,49 +1,14 @@
 <?php
 class Employee
     {
-        private $name;//Ім'я
-        private $age ;//Вік
+        public readonly string $name;
+        public readonly string $surname;      
         private $salary;//зарплата
 
-        public function __construct($a,$b,$c) //конструктор
+        public function __construct($a,$b,$c)
         {
             $this->name=$a;
-            $this->age=$b;
+            $this->surname=$b;
             $this->salary=$c;
         }
-        public function getName() //повернення ім'я
-            {return $this->name;}
-
-        public function getAge() //повернення віку
-            {return $this->age;}
-
-        public function getSalary() // повернення рівня зп
-            {return $this->salary;}
-
-        public function checkAge() //перевірка на повноліття
-            {
-                if ($this->age > 18) return TRUE;
-                   else return FALSE;
-            }
-
-        public function doubleSalary()
-            {$this->salary=$this->salary*2;}
-
-        public function getter($name)
-            {
-                if ($name=='salary') return $this->salary."$";
-                    else return $this->$name;
-            }
-
-        public function setter ($name,$value)
-            {
-                if ($name=='age') $value=$this->isAgeCorrect($value);
-                $this->$name=$value;
-            }
-
-        public function isAgeCorrect($age)
-            {
-                if (($age>1) and ($age<100)) return $age;
-                    else return $this->age;
-            }
     }
--- index.php
+++ index.php
@@ -1,48 +1,7 @@
 <?php
 
-
 require_once('employee.php');
-require_once('user.php');
-require_once('user_user.php');
-require_once('rectangle.php');
 require_once('student.php');
 
-$stud[1]= new Student;
-$stud[1]->name="Петро";
-$stud[1]->course=2;
-
-$stud[2]= new Student;
-$stud[2]->name="Ігор";
-$stud[2]->course=5;
-
-for($i=1;$i<=count($stud);$i++)
-    {
-        echo $stud[$i]->name.' з '.$stud[$i]->course." курсу переведено на ";
-        $stud[$i]->transferToNextCourse();
-        echo $stud[$i]->course." курс <br>";
-    }
-
-//8,9
-$workers[]= new Employee('eric',25,1000);
-$workers[]= new Employee('kyle',30,2000);
-
-//10
-$summ=0;
-for($i=0;$i<count($workers);$i++)
-    {
-        $summ=$summ+$workers[$i]->getSalary();
-    }
-
-echo "<br> Сумарна заробітна плата ".$summ." грн.<br>";
-
-//13
-$workers[1]->setter('age',-2);
-echo $workers[1]->getter('name')." йому ".$workers[1]->getter('age')."<br>";
-
-$workers[1]->setter('age',102);
-echo $workers[1]->getter('name')." йому ".$workers[1]->getter('age')."<br>";
-
-$workers[1]->setter('age',50);
-echo $workers[1]->getter('name')." йому ".$workers[1]->getter('age')."<br>";
-
-echo $workers[0]->getter('name')." отримує ".$workers[0]->getter('salary')."<br>";
+$workers[]= new Employee('Іван',"Петрович",1000);
+$workers[]= new Employee('Ніна',"Володимирівна",2000);
