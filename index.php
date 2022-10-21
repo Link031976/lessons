2ef19b9027a4c22b6a74ee46368358ea1657ccb9
Alex <link_igor@i.ua>
2022-10-21 20:07:44 +0300

    finish v221021

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 require_once('employee.php');
 require_once('student.php');
 
-$workers[]= new Employee('Іван',"Петрович",1000);
-$workers[]= new Employee('Ніна',"Володимирівна",2000);
+$workers[] = new Employee('Іван',"Петрович",1000);
+$workers[] = new Employee('Ніна',"Володимирівна",2000);
