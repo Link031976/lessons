540709b56f5b09b6e580c6f7eda99204509457a4
Alex <115179267+Link031976@users.noreply.github.com>
2022-10-30 11:34:04 +0200

    Update arr.php

--- arr.php
+++ arr.php
@@ -1,25 +1,26 @@
 <?php
 class Arr
-{
-    /**  Место хранения перегружаемых данных.  */
+{    
     private $numbers = array();
     private $index = 0;
 
     public function set($value)
-    {
-        $this->numbers[$this->index] = $value;
-        $this->index++;
-    }
+        {
+            $this->numbers[$this->index] = $value;
+            $this->index++;
+        }
+    
     public function add ($mas)
-    {
-        $n=count($mas);
-        for ($i=0;$i< $n;$i++)
-            {
-                $this->numbers[]=$mas[$i];               
-                $this->index++;
-            }
-        $this->index--;
-    }  
+       {
+            $n=count($mas);
+            for ($i=0;$i< $n;$i++)
+                {
+                    $this->numbers[]=$mas[$i];               
+                    $this->index++;
+                }
+            $this->index--;
+        }  
+    
     public function get()
         {
             echo "<pre>";
@@ -27,10 +28,10 @@ class Arr
         }    
     
     public function getAvg()
-    {
-        $sum=0;
-        for ($i=0;$i<= $this->index;$i++)
-            $sum=$sum + $this->numbers[$i];
-        return array_sum($this->numbers)/$this->index;
-    }
-    }
+        {
+            $sum=0;
+            for ($i=0;$i<= $this->index;$i++)
+                $sum=$sum + $this->numbers[$i];
+            return array_sum($this->numbers)/$this->index;
+        }
+}
